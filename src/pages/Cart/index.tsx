37314078b469fa7542a77034d3541256b07ed9f1
Matheus Matiazzo <mathmatiazzo@gmail.com>
2023-09-16 12:47:44 -0300

    feat: styles cart page

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,21 +1,55 @@
-import { CartContainer, ProductsOnCart } from './styles';
+import { MapPinLine } from 'phosphor-react';
+import expresso from '../../assets/coffee-types/expresso.svg';
+import {
+	CartContainer,
+	OrderAddress,
+	OrderForm,
+	OrderInfo,
+	OrderInfoInput,
+	OrderInput,
+	OrderPaymentMethod,
+	OrderSummary,
+} from './styles';
 
 export function Cart() {
 	return (
 		<CartContainer>
-			<form>
-				<div>
-					<p>Endereço de entrega</p>
-					<span>Informe o endereço onde deseja receber seu pedido</span>
-				</div>
-				<ProductsOnCart>
-					<img src="" alt="" />
+			<OrderForm>
+				<OrderInfo>
+					<h3>Complete seu pedido</h3>
+					<OrderAddress>
+						<p>Endereço de entrega</p>
+						<span>Informe o endereço onde deseja receber seu pedido</span>
+						<MapPinLine />
+						<OrderInput>
+							<OrderInfoInput type="text" placeholder="CEP" />
+							<OrderInfoInput type="text" placeholder="Rua" />
+							<div>
+								<OrderInfoInput type="text" placeholder="Número" />
+								<OrderInfoInput type="text" placeholder="Complemento" />
+							</div>
+							<div>
+								<OrderInfoInput type="text" placeholder="Bairro" />
+								<OrderInfoInput type="text" placeholder="Cidade" />
+								<OrderInfoInput type="text" placeholder="UF" />
+							</div>
+						</OrderInput>
+					</OrderAddress>
+					<OrderPaymentMethod>
+						<span>cartão de crédito</span>
+						<span>Cartão de débito</span>
+						<span>Dinheiro</span>
+					</OrderPaymentMethod>
+				</OrderInfo>
+				<OrderSummary>
+					<h3>Cafés Selecionados</h3>
+					<img src={expresso} alt="" />
 					<div>
 						<span>Espresso Tradicional</span>
 					</div>
 					<span>9,90</span>
-				</ProductsOnCart>
-			</form>
+				</OrderSummary>
+			</OrderForm>
 		</CartContainer>
 	);
 }
--- src/pages/Cart/styles.ts
+++ src/pages/Cart/styles.ts
@@ -2,9 +2,51 @@ import { styled } from 'styled-components';
 
 export const CartContainer = styled.div`
 	width: 100%;
-	max-width: 1120px;
+	max-width: 1300px;
 	margin: 4rem auto 0;
-	padding: 0 1.5rem;
 `;
 
-export const ProductsOnCart = styled.div``;
+export const OrderForm = styled.form`
+	display: flex;
+	align-items: flex-start;
+	justify-content: center;
+	gap: 1rem;
+`;
+
+export const OrderInfo = styled.div`
+	border-radius: 6px;
+	background: ${(props) => props.theme['base-card']};
+	padding: 2rem;
+`;
+
+export const OrderInput = styled.div`
+	display: flex;
+	flex-direction: column;
+	gap: 1rem;
+	margin-top: 1rem;
+
+	div {
+		display: flex;
+		gap: 1rem;
+	}
+`;
+
+interface IInputSizeOnContainer {
+	variant: string;
+}
+
+export const OrderInfoInput = styled.input`
+	background: ${(props) => props.theme['base-input']};
+	padding: 10px 0 10px 0;
+	border: 1px solid ${(props) => props.theme['base-button']};
+	border-radius: 6px;
+	text-indent: 0.8rem;
+
+	/* max-width: ${(props) => props.variant}; */
+`;
+
+export const OrderSummary = styled.div``;
+
+export const OrderAddress = styled.div``;
+
+export const OrderPaymentMethod = styled.div``;
